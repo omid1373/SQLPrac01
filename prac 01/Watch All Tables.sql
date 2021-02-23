54427f29201efaf030d636a0871cb00d79ed82d8
unknown <Administrator@Havades2-pc.fanni.tazarv.com>
2021-02-23 12:46:59 -0800

    Inspector Added :triumph:

---
+++ prac 01/Watch All Tables.sql
@@ -0,0 +1,50 @@
+use MYDB
+go
+select 'Table_Name :  -------------- schools'
+go
+select * from schools
+go
+select 'Table_Name :  -------------- students'
+go
+select * from students
+go
+select 'Table_Name :  -------------- teachers'
+go
+select * from teachers
+go
+select 'Table_Name :  -------------- courses'
+go
+select * from courses
+go
+select 'Table_Name :  -------------- exams'
+go
+select * from exams
+go
+select 'Table_Name :  -------------- terms'
+go
+select * from terms
+go
+select 'Table_Name :  -------------- student_courses'
+go
+select * from student_courses
+go
+select 'Table_Name :  -------------- teacher_courses'
+go
+select * from teacher_courses
+go
+select 'Table_Name :  -------------- school_teachers'
+go
+select * from school_teachers
+go
+select 'Table_Name :  -------------- course_terms'
+go
+select * from course_terms
+go
+select 'Table_Name :  -------------- course_exams'
+go
+select * from course_exams
+go
+select 'Table_Name :  -------------- student_scores'
+go
+select * from student_scores
+go
