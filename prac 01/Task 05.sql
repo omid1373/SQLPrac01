b47ada4788eb691683632d5d0683bfeb198a424f
unknown <Administrator@Havades2-pc.fanni.tazarv.com>
2021-02-24 12:21:44 -0800

    Task 05 Completed Triumphly triumph:

--- prac 01/Task 05.sql
+++ prac 01/Task 05.sql
@@ -3,14 +3,19 @@
 */
 use MYDB
 go
-declare @termId int
-declare @studentId int
-set @termId = 1;
-set @studentId = 1;
 
-select ct.CourseId , AVG(ss.Grade) as Average from course_terms as ct
+create proc TermicAvg05 @termId int
+as
+select sc.StudentId ,SUM(ss.Grade*c.Credit)/SUM(c.Credit) as TermicAverage from student_courses as sc
+inner join course_terms as ct on ct.CourseTermId = sc.CourseTermId
 inner join courses as c on c.CourseId = ct.CourseId
-inner join student_courses as sc on sc.CourseTermId = ct.CourseId
 inner join student_scores as ss on ss.StudentCourseId = sc.StudentCourseID
-where ct.TermId = @termId and sc.StudentId = @studentId
-group by ct.CourseId
+where ct.TermId = @termId
+group by sc.StudentId
+go 
+
+declare @termId int
+set @termId = 1;
+
+
+exec TermicAvg05 @termId;
