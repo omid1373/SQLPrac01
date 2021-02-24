5c6421b2c1ab7b645c4a3fcec6f7c6b2f241764f
unknown <Administrator@Havades2-pc.fanni.tazarv.com>
2021-02-24 14:20:56 -0800

    almost finished :weary:

---
+++ prac 01/Task 08.sql
@@ -0,0 +1,27 @@
+/*
+ãÚÏá ÓÇáÇäå
+*/
+use MYDB
+go
+create proc AnnualAvg08 @studentId int, @year int
+
+as
+
+go
+
+
+use MYDB
+go
+declare @year int
+declare @studentId int
+set @studentId = 2;
+set @year = 2019;
+
+select sc.StudentId , SUM(ss.Grade * c.Credit)/SUM(c.Credit) as Average
+from student_courses as sc 
+inner join student_scores as ss on ss.StudentCourseId = sc.StudentCourseID
+inner join course_terms as ct on ct.CourseTermId = sc.CourseTermId
+inner join terms as t on t.TermID = ct.TermId
+inner join courses as c on c.CourseId = ct.CourseId
+where sc.StudentId = @studentId and t.Year = @year
+group by sc.StudentId
