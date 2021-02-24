428ec5a69cb3bd175e5d309095f244856bfe9294
unknown <Administrator@Havades2-pc.fanni.tazarv.com>
2021-02-24 13:13:18 -0800

    task 07 completed :weary:

--- prac 01/Task 06.sql
+++ prac 01/Task 06.sql
@@ -3,13 +3,21 @@
 */
 use MYDB
 go
-declare @teacherId int
-declare @termId int
-set @teacherId = 2;
-set @termId = 1;
 
-select c.* from courses as c
+create proc TeacherCourses06 @termId int, @TeacherId int
+as
+select tc.TeacherId  ,ct.TermId , c.CourseId , c.Name , c.Book , c.Credit
+,tc.CreatedAt , tc.UpdatedAt
+from courses as c
 inner join teacher_courses as tc on c.CourseId = tc.CourseId
 inner join course_terms as ct on ct.CourseId = c.CourseId
 where tc.TeacherId = @teacherId 
 	and ct.TermId = @termId
+go
+
+declare @teacherId int
+declare @termId int
+set @teacherId = 3;
+set @termId = 4;
+
+exec TeacherCourses06 @termId , @TeacherId
--- prac 01/Task 07.sql
+++ prac 01/Task 07.sql
@@ -1,6 +1,7 @@
 /* -------------------Task 7
 ò«—‰«„Â œ—”?
 */
+/*
 use MYDB
 go
 declare @termId int
@@ -11,3 +12,31 @@ inner join course_exams as ce on ce.CourseTermId = ct.CourseTermId
 inner join student_scores as ss on ss.CourseExamId = ce.CourseExamId
 where ct.TermId = @termId
 group by ct.CourseId
+*/
+use MYDB
+go
+
+create procedure AvgCourse07 @courseId int , @termId int
+as
+select sc.StudentId ,SUM(ss.Grade * c.Credit)/SUM(c.Credit) as Average
+from student_scores as ss 
+inner join student_courses as sc on sc.StudentCourseId = ss.StudentCourseId 
+inner join course_terms as ct on sc.CourseTermId = (
+	select top 1 ct.CourseTermId from course_terms as ct 
+	where ct.CourseId = @courseId and ct.TermId = @termId
+	order by ct.CreatedAt desc)
+inner join courses as c on c.CourseId = @courseId
+group by sc.StudentId
+go
+
+
+
+use MYDB
+go
+declare @courseId int
+declare @termId int
+set @courseId = 1;
+set @termId = 2;
+
+
+exec AvgCourse07 @courseId , @termId;
