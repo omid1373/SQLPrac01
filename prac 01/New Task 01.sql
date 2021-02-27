9a1a758aeac1cc2b6d172ee42cb27da3bae970ee
unknown <Administrator@Havades2-pc.fanni.tazarv.com>
2021-02-27 11:21:13 -0800

    New Task 01 Added to story :wink:

---
+++ prac 01/New Task 01.sql
@@ -0,0 +1,37 @@
+use MYDB
+go 
+create proc CV01 @year int, @studentId int
+as
+begin
+	declare @total nvarchar(20)
+	declare @CV nvarchar(10)
+	set @total = 'Total Average :';
+	set @CV = '---CV---';
+	
+	select * from students where StudentId = @studentId;
+	with cte_CV
+	as(
+	select c.Name as CourseName ,c.Book, AVG(ss.Grade) as Grade , c.Credit , t.TermNumber
+	from student_scores as ss
+		inner join student_courses as sc on sc.StudentCourseId = ss.StudentCourseId
+		inner join students as s on s.StudentId = sc.StudentId
+		inner join course_terms as ct on ct.CourseTermId = sc.CourseTermId
+		inner join courses as c on c.CourseId = ct.CourseId
+		inner join terms as t on t.TermID = ct.TermId
+		where t.Year = @year and s.StudentId = @studentId
+		group by c.CourseId , c.Credit ,c.Name ,c.Book, t.TermNumber
+	)
+	select * from cte_CV 
+	union 
+	select @CV , @total , SUM(Grade*Credit)/SUM(Credit) , null , null  from cte_CV
+end
+go
+
+use MYDB
+go 
+declare @year int
+declare @studentId int
+set @year = 2019;
+set @studentId = 1;
+
+exec CV01 @year , @studentId;
